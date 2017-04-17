96f3ca4126ba15a905ed743cd2843b340ebd17b8
Treker Battig <BattigCA1@gcc.edu>
2017-04-17 16:34:20 -0400

    Switched from WPF to Windows Forms.

--- GUI/Properties/Resources.Designer.cs
+++ GUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Chess.Properties
+namespace GUI.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace Chess.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Chess.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
